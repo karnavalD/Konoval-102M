413d8792383cb1a157c65f0a9a0a697efcfa46f9
karnavalD <106261779+karnavalD@users.noreply.github.com>
2022-05-25 19:01:00 +0300

    SZ2

---
+++ SZ2.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[23]:
+
+
+
+# объявляем переменные
+var_1 = 5
+var_2 = 6
+# выполняем проверку условия
+if var_1 == var_2:
+# если True
+    print(var_1)
+# иначе если выполняется другое условие
+elif var_1 < var_2:
+# если True для elif
+    print(var_1)
+# если False для всех
+else:
+    print(var_2)
+ 
+
+
+var_1 = 7
+var_2 = 8
+var_3 = 9
+
+# выполняем проверку условия
+if var_1 == var_2== var_3:
+# если True
+    print(var_1)
+# иначе если выполняется другое условие
+elif var_1== var_3 < var_2:
+# если True для elif
+    print(var_1)
+elif var_1== var_2 < var_3:
+# если True для elif
+    print(var_1)
+elif var_2== var_3 < var_1:
+# если True для elif
+    print(var_2)  
+elif var_1 < var_2:
+# если True для elif
+    print(var_1) 
+elif var_2 < var_3:
+# если True для elif
+    print(var_1)     
+# если False для всех
+else:
+    print(var_2)
+ 
+
+# объявляем переменные
+var_1 = 2
+var_2 = 1
+var_3 = 2
+
+# выполняем проверку условия
+if var_1 == var_2== var_3:
+# если True
+    print("3")
+# иначе если выполняется другое условие
+elif var_1== var_2:
+# если True для elif
+    print("2")
+elif var_1== var_3 :
+# если True для elif
+    print("2")
+elif var_2== var_3:
+# если True для elif
+    print("2")      
+# если False для всех
+else:
+    print("0")
+
+
+# In[ ]:
+
+
+
+
